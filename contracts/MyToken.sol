a2cad87537606be242253908d7874f9b4b1808c8
minniejung <jungmignonne@gmail.com>
2025-04-08 13:42:41 +0900

    solved

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,1 +1,10 @@
-contract MyToken {}
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MKT") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
