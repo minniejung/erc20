7f932e62e8121eb6ee091b2ff9f136f267c206c6
minniejung <jungmignonne@gmail.com>
2025-04-09 10:44:38 +0900

    fix: config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,12 +7,14 @@ const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
     ganache: {
-      // url: "HTTP://127.0.0.1:7545", // Todo: Ganache RPC URL
-      url: "https://public-en-kairos.node.kaia.io",
+      url: "HTTP://127.0.0.1:7545", // Todo: Ganache RPC URL
       accounts: [
-        // process.env.PRIVATE_KEY_GANACHE || "", // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
-        process.env.PRIVATE_KEY_METAMASK || "",
+        process.env.PRIVATE_KEY_GANACHE || "", // Todo: Ganache에서 제공하는 프라이빗 키 사용(.env 파일을 사용합니다)
       ],
+    },
+    kaia: {
+      url: "https://public-en-kairos.node.kaia.io",
+      accounts: [process.env.PRIVATE_KEY_METAMASK || ""],
     },
   },
 };
